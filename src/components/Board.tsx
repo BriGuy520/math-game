e20b85a6b96af0fe312bb93f129623ed2ba0e994
Brian Falasz <bfalasz@yahoo.com>
2023-06-13 20:52:31 -0500

    have the values displaying correctly

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,18 +3,72 @@ import React from 'react';
 import Input from './Input';
 
 export type BoardProps = {
-  handleGuess: React.ChangeEventHandler<HTMLInputElement>,
-  checkGuess: React.KeyboardEventHandler<HTMLInputElement>,
-  guess?: string,
   operators: string[],
 }
 
-const Board = ({guess, operators, checkGuess, handleGuess}: BoardProps) => {
+const Board = ({operators}: BoardProps) => {
 
+  const [firstValue, setFirstValue] = React.useState(Math.floor(Math.floor(Math.random() * 100)));
+  const [secondValue, setSecondValue] = React.useState(Math.floor(Math.floor(Math.random() * 100)));
+
+  const [guess, setGuess] = React.useState<string>("");
+
+  const selectedOperator: string = operators[Math.floor(Math.random() * operators.length)];
+  let displayOperator: string = selectedOperator == "Addition" ? "+" : (selectedOperator == "Substraction" ? "-" : (selectedOperator == "Multiplication" ? "x" : "/"));
+  let answer: number;
+
+
+  
+  if(selectedOperator === "Addition"){
+    displayOperator = "+";
+  } else if(selectedOperator === "Subtraction"){
+    displayOperator = "-";
+  } else if(selectedOperator === "Multiplication"){
+    displayOperator = "x";
+  } else if(selectedOperator === "Divsion"){
+    displayOperator = "/";
+  }
+
+
+    /* Game */
+
+  const handleGuess = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setGuess(e.target.value);
+  }
+
+  const checkGuess = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+    if(e.key === 'Enter'){
+
+      switch(selectedOperator){
+
+        case 'Addition':
+          answer = firstValue + secondValue;
+          break;
+        case 'Subtraction':
+          answer = firstValue - secondValue;
+          break;
+        case 'Multiplication':
+          answer = firstValue * secondValue;
+          break;
+        default:
+          answer = firstValue / secondValue;  
+          break;
+      }
+  
+      console.log(answer);
+    }
+
+  }
+
+  console.log(displayOperator);
 
   return (
+    
     <div className="board-container">
-      <p>{Math.floor(Math.random() * 100)}</p>
+      <p>{firstValue} {displayOperator} {secondValue}</p>
       <Input handleChange={handleGuess} checkGuess={checkGuess} inputValue={guess} />
     </div>
   );
