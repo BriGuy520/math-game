7361ffbe900792f50853831a44d8e3ac71ff9b8f
Brian Falasz <bfalasz@yahoo.com>
2023-06-16 22:14:19 -0500

    added a scores component to keep track of the high scores

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Timer, { TimerProps } from './Timer';
 import Board, { BoardProps } from './Board';
+import Scores from './Scores';
 
 type GameProps = TimerProps & BoardProps;
 
@@ -12,6 +13,7 @@ const Game = ({timeLeft, operators, stopGame, startGame, startTimer}: GameProps)
     <>
       <Timer timeLeft={timeLeft} startGame={startGame} stopGame={stopGame} />
       <Board timeLeft={timeLeft} startTimer={startTimer} operators={operators} />
+      <Scores />
     </>
   )
 }
---
+++ src/components/Scores.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Scores = () => {
+
+
+  return (
+    <div className="scores-container"> 
+      <h4>High Scores</h4>
+      <ol>
+        <li></li>
+      </ol>
+    </div>
+  );
+
+}
+
+export default Scores;
